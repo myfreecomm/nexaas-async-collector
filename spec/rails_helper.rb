2dabb416a7105a7734562e0578b19e4df0eedabf
Eduardo Hertz <eduardohertz@gmail.com>
2017-06-07 11:06:20 -0300

    Remove comment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-# require "sqlite3"
-
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 
 Dir[File.join File.dirname(__FILE__), 'support', '**', '*.rb'].each { |f| require f }
