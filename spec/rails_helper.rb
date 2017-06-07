ad7aeb6eecca3f2c97c7da3fdce7ec4a297e22d2
Eduardo Hertz <eduardohertz@gmail.com>
2017-06-07 10:07:15 -0300

    Fix README and remove useless spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 
-require "sqlite3"
+# require "sqlite3"
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 
